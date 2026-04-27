d527a9e5029db751bfeb54d1c1b0878b80538517
nayanmdgit <nayandc2016@gmail.com>
2026-04-27 21:41:50 +0600

    All Countries from API is shown

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,15 +1,19 @@
 
 import { use } from 'react';
+import Country from '../Country/Country';
 
 const Countries = ({countriesPromise}) => {
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries
-    console.log(countries);
+  
 
 
     return (
         <div>
             <h1>Counties in the World</h1>
+            {
+                countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+            }
         </div>
     );
 };
