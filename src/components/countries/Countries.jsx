2fa36d0b4de0c68f979c98992758a87544ec148c
nayanmdgit <nayandc2016@gmail.com>
2026-04-27 18:03:04 +0600

    first commit

---
+++ src/components/countries/Countries.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Counties in the World</h1>
+        </div>
+    );
+};
+
+export default Countries;
